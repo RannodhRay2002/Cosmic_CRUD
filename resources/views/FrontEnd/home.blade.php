3d91a0c81ad03b3fb22c9222569877c10ce3c07a
Ray Fernando <107664151+RannodhRay2002@users.noreply.github.com>
2023-09-07 17:04:35 +0530

    ssp2

---
+++ resources/views/FrontEnd/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="stylesheet" href="public\css\cosmic.css">-->
+    <link rel="stylesheet" href="{{asset('css/cosmic.css')}}">
+    <title>Cosmic.co</title>
+    <style>
+        .banner{
+    width:100%;
+    height:100vh;
+    background-image: linear-gradient(rgba(0,0,0,0.50),rgba(0,0,0,0.50)),url('Images/background.jpg');
+    background-size: cover;
+    background-position: center;
+}
+    </style>
+
+</head>
+<body>
+    <div class="banner">
+        <div class="navbar">
+            <a href="index.html">
+            <img src="Images\HatchfulExport-All\logo-removebg-preview.png" alt="logo" class="logo">
+            </a>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="watchesmod.html">Explore</a></li>
+                <li><a href="hublot.html">Hublot</a></li>
+                <li><a href="rolex.html">Rolex</a></li>
+                <li><a href="jacobmod.html">Jacob & Co</a></li>
+                <li><a href="login.html">Log in</a></li>
+                <li><a href="Signup.html">Sign up</a></li>
+            </ul>
+            
+        </div>
+        <div class="content">
+            <h1>Cosmic Opulence</h1>
+            <p>Dive into a timeless cosmos</p>               
+            <div class="banner-btn">
+                
+              <!--  <button type="button"><span></span>Explore Watches</button>
+                <button type="button"><span></span>Lorem</button>-->
+                <a href="watchesmod.html"><span></span>Explore Watches</a>
+                <a href="https://en.wikipedia.org/wiki/Watch"><span></span>Read more</a>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
+
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+  <img src="/Images/HatchfulExport-All/logo-removebg-preview.png" class="w-35 h-40" alt="Logo">
+</a>
